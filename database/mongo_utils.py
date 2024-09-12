d5d81b4c79375b09a8650e3c0c7d21756e17e43d
YOYOStar <3607779143@qq.com>
2024-09-13 02:29:43 +0800

    新增条件，必须"created_at"的时间为一周之内的数据

--- database/mongo_utils.py
+++ database/mongo_utils.py
@@ -1,5 +1,6 @@
+import datetime
 from pymongo import MongoClient
-from . import config
+import config
 
 
 def connect_to_mongo(db_name, collection_name, mongo_uri):
@@ -10,12 +11,25 @@ def connect_to_mongo(db_name, collection_name, mongo_uri):
 
 
 def get_candidates_from_mongo():
+    # Connect to the MongoDB collection
     collection = connect_to_mongo(
         db_name=config.db_name,
         collection_name=config.cv_collection_name,
         mongo_uri=config.mongo_uri,
     )
+
+    # Calculate the date one week ago from now
+    one_week_ago = datetime.datetime.now() - datetime.timedelta(days=7)
+
+    # Query to find candidates created within the last week
     candidates = list(
-        collection.find({}, {"_id": 0, "jobid": 1, "username": 1, "userid": 1})
+        collection.find(
+            {"created_at": {"$gte": one_week_ago.strftime("%Y-%m-%d %H:%M:%S")}},
+            {"_id": 0, "jobid": 1, "username": 1, "userid": 1},
+        )
     )
     return candidates
+
+
+if __name__ == "__main__":
+    print(get_candidates_from_mongo())

eb4c317e426eed15e616e301388c57d36181949a
YOYOStar <3607779143@qq.com>
2024-09-13 02:28:52 +0800

    取消注释掉的代码

--- req.py
+++ req.py
@@ -124,40 +124,40 @@ def run_tests():
     url = "http://localhost:8000/"
     req = Request(url)
 
-    # test_cases = [
-    #     {"method": "post", "route": "work-change", "data": {"jobid": 5}},
-    #     {
-    #         "method": "post",
-    #         "route": "trade",
-    #         "data": {"merchantid": 1, "merchantnum": 1, "transactiontype": 0},
-    #     },
-    #     {"method": "post", "route": "use", "data": {"merchantid": 2, "merchantnum": 1}},
-    #     {"method": "post", "route": "see-doctor", "data": {}},
-    #     {"method": "post", "route": "sleep", "data": {"timelength": 10}},
-    #     {"method": "get", "route": "activity", "data": {"subjectid": 4}},
-    #     {"method": "get", "route": "position", "data": None},
-    # ]
+    test_cases = [
+        {"method": "post", "route": "work-change", "data": {"jobid": 5}},
+        {
+            "method": "post",
+            "route": "trade",
+            "data": {"merchantid": 1, "merchantnum": 1, "transactiontype": 0},
+        },
+        {"method": "post", "route": "use", "data": {"merchantid": 2, "merchantnum": 1}},
+        {"method": "post", "route": "see-doctor", "data": {}},
+        {"method": "post", "route": "sleep", "data": {"timelength": 10}},
+        {"method": "get", "route": "activity", "data": {"subjectid": 4}},
+        {"method": "get", "route": "position", "data": None},
+    ]
 
-    # for test in test_cases:
-    #     method = test["method"]
-    #     route = test["route"]
-    #     data = test["data"]
+    for test in test_cases:
+        method = test["method"]
+        route = test["route"]
+        data = test["data"]
 
-    #     if method == "post":
-    #         response = req.post(route, data)
-    #     elif method == "get":
-    #         response = req.get(route, data)
+        if method == "post":
+            response = req.post(route, data)
+        elif method == "get":
+            response = req.get(route, data)
 
-    #     print(f"Testing {method.upper()} {route}")
-    #     print("Status Code:", response.status_code)
-    #     print("Response Body:", response.json())
-    #     print("-" * 40)
+        print(f"Testing {method.upper()} {route}")
+        print("Status Code:", response.status_code)
+        print("Response Body:", response.json())
+        print("-" * 40)
 
-    # test_work_change()
-    # test_resume_submission()
-    # test_vote()
-    # test_public_job()
-    # test_freelance_job()
+    test_work_change()
+    test_resume_submission()
+    test_vote()
+    test_public_job()
+    test_freelance_job()
     test_get_candidates()
 
 

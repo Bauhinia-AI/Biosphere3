06d4efa666e9c614ef4a4756b272fa52f7ee85fd
YOYOStar <3607779143@qq.com>
2024-09-13 02:07:03 +0800

    mongo_utils.py: 添加一个新函数来从MongoDB数据库中获取候选人信息。

--- database/mongo_utils.py
+++ database/mongo_utils.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from . import config
 
 
 def connect_to_mongo(db_name, collection_name, mongo_uri):
@@ -6,3 +7,15 @@ def connect_to_mongo(db_name, collection_name, mongo_uri):
     db = client[db_name]
     collection = db[collection_name]
     return collection
+
+
+def get_candidates_from_mongo():
+    collection = connect_to_mongo(
+        db_name=config.db_name,
+        collection_name=config.cv_collection_name,
+        mongo_uri=config.mongo_uri,
+    )
+    candidates = list(
+        collection.find({}, {"_id": 0, "jobid": 1, "username": 1, "userid": 1})
+    )
+    return candidates

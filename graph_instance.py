3d39f0fb553e6041c70158b844e6dfb3b1d2ae38
rickyds97 <rickycs97@pku.edu.cn>
2024-10-26 17:17:31 +0800

    事件调度系统、终止信号监听、感知路由

--- graph_instance.py
+++ graph_instance.py
@@ -5,109 +5,56 @@ from agent_srv.node_model import RunningState
 from langgraph.graph import StateGraph, START, END
 import os
 import asyncio
-import pprint
+from pprint import pprint
+from agent_srv.utils import generate_initial_state, check_termination
 
 
-tool_functions_easy = """
-    4.	pick_apple(): Pick an apple, costing energy.
-Constraints: Must have enough energy and be in the orchard.\n
-	5.	go_fishing(): Fish for resources, costing energy.
-Constraints: Must have enough energy and be in the fishing area.\n
-	6.	mine(): Mine for resources, costing energy.
-Constraints: Must have enough energy and be in the mine.\n
-	7.	harvest(): Harvest crops, costing energy.
-Constraints: Must have enough energy and be in the harvest area.\n
-	8.	buy(itemType: ItemType, amount: int): Purchase items, costing money.
-Constraints: Must have enough money, and items must be available in sufficient quantity in the AMM. ItemType:(Ore,Bread,Apple,Wheat,Fish)\n
-	9.	sell(itemType: ItemType, amount: int): Sell items for money.
-Constraints: Must have enough items in inventory.ItemType:(Ore,Bread,Apple,Wheat,Fish)\n
-	10.	use_item(itemType: ItemType, amount: int): Use an item.
-Constraints: Must have enough items in inventory.ItemType:(Ore,Bread,Apple,Wheat,Fish)\n
-	11.	see_doctor(hours: int): Visit a doctor, costing money.
-Constraints: Must have enough money and be in the hospital.\n
-	12.	sleep(hours: int): Sleep to recover energy and health.
-Constraints: Must be at home.\n
-    13.	study(hours: int): Study to achieve a higher degree.
-Constraints: Must be in school and have enough money.\n
-    14.	nav(placeName: string): Navigate to a specified location.
-Constraints: Must in (school,workshop,home,farm,mall,square,hospital,fruit,harvest,fishing,mine,orchard).
-"""
 
 
-def generate_initial_state(websocket):
-    initial_state = {
-        "userid": 12,
-        "character_stats": {
-            "name": "Alice",
-            "gender": "Female",
-            "slogan": "Adventure awaits!",
-            "description": "A brave explorer.",
-            "role": "Explorer",
-            "inventory": {},
-            "health": 100,
-            "energy": 100,
-        },
-        "decision": {
-            "need_replan": False,
-            "action_description": ["I successfully picked a banana."],
-            "action_result": [],
-            "new_plan": [],
-            "daily_objective": [],
-            "meta_seq": [],
-            "reflection": ["Nice"],
-        },
-        "meta": {
-            "tool_functions": tool_functions_easy,
-            "day": "Monday",
-            "available_locations": [
-                "school",
-                "workshop",
-                "home",
-                "farm",
-                "mall",
-                "square",
-                "hospital",
-                "fruit",
-                "harvest",
-                "fishing",
-                "mine",
-                "orchard",
-            ],
-        },
-        "message_queue": asyncio.Queue(),
-        "websocket": websocket,
-    }
-    return initial_state
 
 
 class LangGraphInstance:
     def __init__(self, user_id, websocket=None):
         self.user_id = user_id
         self.websocket = websocket
+        self.signal = None
         # 初始化 langgraph 实例
-        # 根据user_id 检索数据库中的信息，更新stat
-        self.state = RunningState(**generate_initial_state(self.websocket))
+        #  TODO We should 根据user_id 检索数据库中的信息，更新stat
+        self.state = RunningState(
+            **generate_initial_state(self.user_id, self.websocket)
+        )
+        self.connection_stats = {}
+        # 数据竞争时，锁住state
+        self.state_lock = asyncio.Lock()
         self.graph = self._get_workflow_with_listener()
+        self.graph_config = {"recursion_limit": 30}
+        # 三个协程
         self.listener_task = asyncio.create_task(self.listener())
-        #self.schedule_task = asyncio.create_task(self.schedule_messages())
-
-        # TODO We should get and init state from database
-        # self.state: RunningState = initialize_running_state(user_id,character_params, decision_params, meta_params)
+        self.msg_processor_task = asyncio.create_task(self.msg_processor())
+        self.event_scheduler_task = asyncio.create_task(self.event_scheduler())
+        self.queue_visulizer_task = asyncio.create_task(self.queue_visulizer())
+        # self.schedule_task = asyncio.create_task(self.schedule_messages())
+        self.state["event_queue"].put_nowait("PLAN")
         logger.info(f"User {self.user_id} workflow initialized")
         self.task = asyncio.create_task(self.a_run())
 
     # 生产者listener，独立于graph运行
+    @check_termination
     async def listener(self):
         websocket = self.state["websocket"]
         message_queue = self.state["message_queue"]
-        logger.info(f"👂 User {self.user_id}: LISTENER started...")
+        # logger.info(f"👂 User {self.user_id}: LISTENER started...")
 
         try:
             async for message in websocket:
                 data = json.loads(message)
-                await message_queue.put(data)
+                async with self.state_lock:
+                    await message_queue.put(data)
+                # logger.info(
+                #     f"👂 User {self.user_id}: Received message: {data} and put into queue"
+                # )
                 logger.info(
-                    f"👂 User {self.user_id}: Received message: {data} and put into queue"
+                    f"🧾 User {self.user_id} message_queue: {self.state['message_queue']}"
                 )
         except websockets.ConnectionClosed:
             logger.error(f"User {self.user_id}: WebSocket connection closed.")
@@ -115,82 +62,95 @@ class LangGraphInstance:
         except Exception as e:
             logger.error(f"User {self.user_id}: Error in listener: {e}")
 
-
-
-    async def send_msg_to_game(self, message_name, data):
-        message = {
-            "messageName": message_name,
-            "data": data,
-            "characterId": self.user_id,
-        }
-        await self.websocket.send(json.dumps(message))
-
-    async def a_run(self):
-        try:
-            await self.graph.ainvoke(self.state)
-        except Exception as e:
-            logger.error(f"User {self.user_id} Error in workflow: {e}")
-
-    async def process_messages(self, state: RunningState):
+    @check_termination
+    async def msg_processor(self):
         while True:
-            message = await state["message_queue"].get()
+            with self.state_lock:
+                msg = await self.state["message_queue"].get()
+            message_name = msg.get("messageName")
 
-            if message is None:
-                logger.error(f"User {self.user_id}: WebSocket connection closed.")
-                break
-            logger.info(f"User {self.user_id}: Received message: {message}")
-
-            message_name = message.get("messageName")
             if message_name == "action_result":
                 # 处理动作结果
-                self.state["decision"]["action_result"].append(message["data"])
+                self.state["decision"]["action_result"].append(msg["data"])
                 logger.info(
-                f"🏃 User {self.user_id}: Received action result: {message['data']}"
-            )
+                    f"🏃 User {self.user_id}: Received action result: {msg['data']}"
+                )
             elif message_name == "gameevent":
                 pass
 
             elif message_name == "onestep":
-                return "Objectives_planner"
-            
+                self.state["event_queue"].put_nowait("PLAN_ONCE")
+
             elif message_name == "check":
-               pprint(self.state["decision"]["actionresult"])
+                pprint(self.state["decision"]["action_result"])
 
             else:
                 logger.error(f"User {self.user_id}: Unknown message: {message_name}")
+
+    @check_termination
+    async def event_scheduler(self):
+        while True:
+            await asyncio.sleep(10)
+            self.state["event_queue"].put_nowait("PLAN")
+            logger.info(f"🆕 User {self.user_id}: Put PLAN into event_queue")
+
+    @check_termination
+    async def a_run(self):
+        try:
+            await self.graph.ainvoke(self.state, config=self.graph_config)
+        except Exception as e:
+            self.signal = "TERMINATE"
+            logger.error(f"User {self.user_id} Error in workflow: {e}")
+    @check_termination
+    async def queue_visulizer(self):
+        while True:
+            await asyncio.sleep(10)
+            logger.info(f"🧾 User {self.user_id} event_queue: {self.state['event_queue']}")
+            logger.info(f"🧾 User {self.user_id} message_queue: {self.state['message_queue']}")
+
+    @check_termination
+    async def event_router(self, state: RunningState):
+        while True:
+            async with self.state_lock:
+                event = await state["event_queue"].get()
+            
+            if event == "PLAN":
+                return "Objectives_planner"
+
+            elif event == "PLAN_ONCE":
+                return "Objectives_planner"
+            elif event == "gameevent":
+                pass
+
+            elif event == "onestep":
+                return "Objectives_planner"
+
+            elif event == "check":
+                pprint(self.state["decision"]["action_result"])
+
+            else:
+                logger.error(f"User {self.user_id}: Unknown event: {event}")
             # await self.state['message_queue'].task_done()
 
     def _get_workflow_with_listener(self):
         workflow = StateGraph(RunningState)
         # workflow.add_node("Process_Messages", self.process_messages)
-        workflow.add_node("")
+        workflow.add_node("Sensing_Route", sensing_environment)
         workflow.add_node("Objectives_planner", generate_daily_objective)
         workflow.add_node("meta_action_sequence", generate_meta_action_sequence)
         workflow.add_node("adjust_meta_action_sequence", adjust_meta_action_sequence)
-
-        workflow.set_conditional_entry_point(self.process_messages)
+        workflow.set_entry_point("Sensing_Route")
+        workflow.add_conditional_edges("Sensing_Route", self.event_router)
+        # workflow.set_conditional_entry_point(self.process_messages)
 
         # 定义工作流的路径
         workflow.add_edge("Objectives_planner", "meta_action_sequence")
         workflow.add_edge(
             "meta_action_sequence", "adjust_meta_action_sequence"
         )  # 循环回消息处理
-        workflow.add_edge("adjust_meta_action_sequence", "")
+        workflow.add_edge("adjust_meta_action_sequence", "Sensing_Route")
 
         return workflow.compile()
 
-    def _get_workflow_without_listener(self):
-        workflow = StateGraph(RunningState)
-        workflow.add_node("Objectives_planner", generate_daily_objective)
-        workflow.add_node("meta_action_sequence", generate_meta_action_sequence)
-        workflow.add_node("adjust_meta_action_sequence", adjust_meta_action_sequence)
-        workflow.set_entry_point("Objectives_planner")
-        workflow.set_finish_point("adjust_meta_action_sequence")
-        workflow.add_edge("Objectives_planner", "meta_action_sequence")
-        workflow.add_edge("meta_action_sequence", "adjust_meta_action_sequence")
-        # workflow.add_conditional_edges("replan", should_end)
-        return workflow.compile()
 
 
-if __name__ == "__main__":
-    asyncio.run(main())

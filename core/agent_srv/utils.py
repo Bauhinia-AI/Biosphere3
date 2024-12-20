c7f44841332750abb27ffaf04f67b8554870784e
Fernando-Zzs <65518148+Fernando-Zzs@users.noreply.github.com>
2024-12-20 16:28:12 +0800

    role_specific_skills

--- core/agent_srv/utils.py
+++ core/agent_srv/utils.py
@@ -336,114 +336,9 @@ async def get_initial_state_from_db(userid, websocket):
         "current_pointer": "Sensing_Route",
     }
     return state
-    # initial_state = {
-    #     "userid": userid,
-    #     "character_stats": {
-    #         "name": "Alice",
-    #         "gender": "Female",
-    #         "slogan": "Need to be rich!Need to be educated!",
-    #         "description": "A risk lover. Always looking for the next big thing.",
-    #         "role": "Investor",
-    #         "inventory": get_inventory(userid),
-    #         "health": 100,
-    #         "energy": 100,
-    #     },
-    # "decision": {
-    #     "need_replan": False,
-    #     "action_description": [],
-    #     "action_result": [],
-    #     "new_plan": [],
-    #     "daily_objective": [],
-    #     "meta_seq": [],
-    #     "reflection": [],
-    # },
-    # "meta": {
-    #     "tool_functions": tool_functions_easy,
-    #     "day": "",
-    #     "available_locations": [
-    #         "school",
-    #         "workshop",
-    #         "home",
-    #         "farm",
-    #         "mall",
-    #         "square",
-    #         "hospital",
-    #         "fruit",
-    #         "harvest",
-    #         "fishing",
-    #         "mine",
-    #         "orchard",
-    #     ],
-    # },
-    # "prompts": {
-    #     "daily_goal": "",
-    #     "refer_to_previous": False,
-    #     "life_style": "Casual",
-    #     "daily_objective_ar": "",
-    #     "task_priority": [],
-    #     "max_actions": 10,
-    #     "meta_seq_ar": "",
-    #     "replan_time_limit": 3,
-    #     "meta_seq_adjuster_ar": "",
-    #     "focus_topic": [],
-    #     "depth_of_reflection": "Moderate",
-    #     "reflection_ar": "",
-    #     "level_of_detail": "Moderate",
-    #     "tone_and_style": "",
-    # },
-    # "public_data": {
-    #     "market_data": market_data_dict,
-    # },
-    # "message_queue": asyncio.Queue(),
-    # "event_queue": asyncio.Queue(),
-    # "false_action_queue": asyncio.Queue(),
-    # "websocket": websocket,
-    # "current_pointer": "Sensing_Route",
-    # }
 
 
 def generate_initial_state_hardcoded(userid, websocket):
-    try:
-        price_response = requests.get(
-            f"{GAME_BACKEND_URL}/ammPool/getAveragePrice", timeout=GAME_BACKEND_TIMEOUT
-        )
-        market_data = price_response.json()["data"]
-        market_data_dict = dict(
-            {
-                x["name"]: x["averagePrice"]
-                for x in market_data
-                if x["name"] in ["ore", "apple", "wheat", "fish"]
-            }
-        )
-    except TimeoutError:
-        logger.error("Failed to get market data from AMM pool")
-
-    # =======
-    # def get_inventory(userid) -> dict:
-    #     # 从数据库中读取http://47.95.21.135:8082/ammPool/getAveragePrice
-    #     response = requests.get(f"http://47.95.21.135:8082/bag/getByCharacterId/{userid}")
-    #     # 只保留ore, apple, wheat, fish
-    #     inventory_dict = {}
-    #     for x in response.json()["data"]:
-    #         if x["itemName"].lower() in ["apple", "wheat", "fish"]:
-    #             inventory_dict[x["itemName"]] = x["itemQuantity"]
-    #         if x["itemName"].lower() == "iron_ore":
-    #             inventory_dict["ore"] = x["itemQuantity"]
-
-    #     return inventory_dict
-
-    # def generate_initial_state_hardcoded(userid, websocket):
-    #     # 从数据库中读取http://47.95.21.135:8082/ammPool/getAveragePrice
-    #     price_response = requests.get("http://47.95.21.135:8082/ammPool/getAveragePrice")
-    #     market_data = price_response.json()["data"]
-    #     market_data_dict = dict(
-    #         {
-    #             x["name"]: x["averagePrice"]
-    #             for x in market_data
-    #             if x["name"] in ["ore", "apple", "wheat", "fish"]
-    #         }
-    #     )
-    # >>>>>>> dev
     initial_state = {
         "userid": userid,
         "character_stats": {
@@ -500,7 +395,7 @@ def generate_initial_state_hardcoded(userid, websocket):
             "tone_and_style": "",
         },
         "public_data": {
-            "market_data": market_data_dict,
+            "market_data": get_market_data_from_db(),
         },
         "message_queue": asyncio.Queue(),
         "event_queue": asyncio.Queue(),
@@ -518,25 +413,23 @@ Constraints: Must in (school,workshop,home,farm,mall,square,hospital,fruit,harve
 Constraints: Must have enough energy and be in the orchard.\n
     3. gofishing [hours:int]: Fish for fish, costing energy.
 Constraints: Must have enough energy and be in the fishing area.\n
-    4. gomining [hours:int]: Mine for ore, costing energy.
-Constraints: Must have enough energy and be in the mine.\n
-    5. harvest [hours:int]: Harvest crops, costing energy.
+    4. harvest [hours:int]: Harvest crops, costing energy.
 Constraints: Must have enough energy and be in the harvest area.\n
-    
-    7. sell [itemType:string] [amount:int]: Sell items for money. The ONLY way to get money.
-Constraints: Must have enough items in inventory. ItemType:(ore,bread,apple,wheat,fish)\n
-    
-    10. study [hours:int]: Study to achieve a higher degree, will cost money.
+    5. sleep [hours:int]: Sleep to recover energy and health.
+Constraints: Must be at home.\n
+    6. study [hours:int]: Study to achieve a higher degree, will cost money.
 Constraints: Must be in school and have enough money.\n
+    8. gomining [hours:int]: Mine for ore, costing energy.
+Constraints: Must have enough energy and be in the mine.\n
+    17. buy [itemType:string] [amount:int]: Purchase items, costing money.
+Constraints: Must have enough money, and items must be available in sufficient quantity in the AMM. ItemType:(ore,bread,apple,wheat,fish)\n
+    18. sell [itemType:string] [amount:int]: Sell items for money. The ONLY way to get money.
+Constraints: Must have enough items in inventory. ItemType:(ore,bread,apple,wheat,fish)\n
+    20. showallitem: Show all items in inventory.
+Constraints: None
+    21. getprice [itemType:string]: Get the price of an item.
+Constraints: None
 """
-# 6. buy [itemType:string] [amount:int]: Purchase items, costing money.
-# Constraints: Must have enough money, and items must be available in sufficient quantity in the AMM. ItemType:(ore,bread,apple,wheat,fish)\n
-
-#     10. sleep [hours:int]: Sleep to recover energy and health.
-# Constraints: Must be at home.\n
-
-# 9. seedoctor [hours:int]: Visit a doctor, costing money.
-# Constraints: Must have enough money and be in the hospital.\n
 
 
 if __name__ == "__main__":

04d20f4c5c1b8f4c50f6878cfd2cb99a052e4c76
YOYOStar <3607779143@qq.com>
2024-12-27 21:13:33 +0800

    character_arc_collection don't use character_id as unique index

--- database/create_database.py
+++ database/create_database.py
@@ -196,9 +196,7 @@ if __name__ == "__main__":
     #     config.intimacy_collection_name, unique_index_fields=["from_id", "to_id"]
     # )
     # app.setup_database(config.knowledge_collection_name)
-    app.setup_database(
-        config.character_arc_collection_name, unique_index_fields=["characterId"]
-    )
+    app.setup_database(config.character_arc_collection_name)
     # app.setup_database(config.profile_sample_collection_name)
     # app.setup_database(
     #     config.agent_prompt_collection_name, unique_index_fields=["characterId"]
